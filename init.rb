bfd0b2751de9809155913387a441564d0c313d28
Artur Trofimov <arturtr@gmail.com>
2016-08-25 15:41:23 +1000

    Update plugin info

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :redmine_telegram_auth do
   name 'Redmine Telegram Auth plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/centosadmin/redmine_telegram_auth'
+  description 'Two factor authorization with Telegram for Redmine'
+  author 'Centos-admin.ru'
+  author_url 'http://centos-admin.ru'
 end
