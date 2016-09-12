1459b6e8367d9d941aa36ed867d119d5b5483f3f
Artur Trofimov <arturtr@gmail.com>
2016-09-12 17:11:35 +1000

    Add message sending by bot

--- lib/redmine_2fa/telegram_auth.rb
+++ lib/redmine_2fa/telegram_auth.rb
@@ -3,7 +3,20 @@ module Redmine2FA
 
 
     def self.send_otp_code(user)
-      puts "AUTH CODE: #{user.otp_code}"
+      auth_method_name = user&.auth_source&.auth_method_name
+
+      if auth_method_name == 'Telegram'
+        telegram_account = user.telegram_account
+
+        token = Setting.plugin_redmine_2fa['bot_token']
+        bot = Telegrammer::Bot.new(token)
+
+        message = "#{user.otp_code}"
+        bot.send_message(chat_id: telegram_account.telegram_id, text: message)
+      else
+        puts "AUTH CODE: #{user.otp_code}"
+      end
+
     end
 
   end
