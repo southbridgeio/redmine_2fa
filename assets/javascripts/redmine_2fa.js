f4bf289ce168784973686a6d6fd59ba144713daf
Artur Trofimov <arturtr@gmail.com>
2016-09-21 15:27:11 +1000

    move startOptTimer to js assets

---
+++ assets/javascripts/redmine_2fa.js
@@ -0,0 +1,24 @@
+function startOtpTimer() {
+  var duration = 30,
+      display = document.querySelector('#otpCodeResendTimer'),
+      timer = duration,
+      seconds,
+      nIntervId;
+
+  $('#otpCodeResendLink').hide();
+  $('#otpCodeResentInstruction').show();
+
+  nIntervId = setInterval(function () {
+
+    seconds = parseInt(timer, 10);
+
+    display.textContent = seconds;
+
+    if (--timer < 0) {
+      $('#otpCodeResendLink').show();
+      $('#otpCodeResentInstruction').hide();
+      display.textContent = duration;
+      clearInterval(nIntervId);
+    }
+  }, 1000);
+}
