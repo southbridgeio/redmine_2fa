ca4d73b10e574977556d51eab8cc57b44866b2b6
Artur Trofimov <arturtr@gmail.com>
2016-10-31 18:00:25 +1000

    Move route test to common

--- test/integration/routing/redmine_telegram_connections_test.rb
+++
@@ -1,6 +0,0 @@
-require File.expand_path('../../../test_helper', __FILE__)
-class RedmineTelegramConnectionsTest < Redmine::RoutingTest
-  def test_redmine_telegram_connections
-    should_route 'GET /telegram/connect' => 'redmine_telegram_connections#create'
-  end
-end
