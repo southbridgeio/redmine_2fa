e42e457908b1a290502bec0a54a846bfd317a152
Artur Trofimov <arturtr@gmail.com>
2016-11-10 17:27:59 +1000

    Cleanup

--- test/integration/routing/account_test.rb
+++ test/integration/routing/account_test.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../../../test_helper', __FILE__)
-class SecondAuthenticationsTest < Redmine::RoutingTest
-  def test_second_authentications
+class AccountTest < Redmine::RoutingTest
+  def test_account_routes
     should_route 'POST /redmine_2fa/confirm' => 'account#confirm_2fa'
     should_route 'POST /redmine_2fa/otp_code/confirm' => 'account#confirm_otp'
   end
