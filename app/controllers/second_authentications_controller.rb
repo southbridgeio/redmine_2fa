98436ed3c6a4f40945f8df2845bbecc01df27a44
cross <cross@vgg.ru>
2017-01-10 14:00:21 +0300

    Fix security bug.

--- app/controllers/second_authentications_controller.rb
+++ app/controllers/second_authentications_controller.rb
@@ -2,8 +2,13 @@ class SecondAuthenticationsController < ApplicationController
   unloadable
 
   def destroy
-    User.find(params[:id]).reset_second_auth
-    flash[:notice] = l(:notice_2fa_reset)
-    redirect_to(:back)
+    user = User.find(params[:id])
+    if User.current.admin? || user == User.current
+      user.reset_second_auth
+      flash[:notice] = l(:notice_2fa_reset)
+      redirect_to(:back)
+    else
+      render_403
+    end
   end
 end
