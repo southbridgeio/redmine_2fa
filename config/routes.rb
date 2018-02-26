e1ca94da6de83aefc748d9ff66b7dd89051eab04
Vladislav Yashin <v.yashin.work@gmail.com>
2018-02-26 16:09:39 +0300

    Fix tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 scope 'redmine_2fa' do
-  # Plugin settings
-
-  post 'bot/init' => 'otp_bot#create', as: 'otp_bot_init'
-  delete 'bot/reset' => 'otp_bot#destroy', as: 'otp_bot_reset'
-
   # 2FA init
   put 'mobile_phone/update', to: 'user_mobile_phone#update', as: 'user_mobile_phone_update'
   post 'mobile_phone/confirm', to: 'user_mobile_phone#confirm', as: 'user_mobile_phone_confirm'
--- test/integration/routing/otp_bot_webhook_test.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path('../../../test_helper', __FILE__)
-
-class OtpBotWebhookTest < Redmine::RoutingTest
-  def test_otp_bot_webhook
-    assert_routing({ method: 'post', path: '/redmine_2fa/bot/token:token/update' },
-                   controller: 'otp_bot_webhook', action: 'update', token: 'token:token')
-  end
-end
