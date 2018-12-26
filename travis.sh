ce37f920b78273e32e40c0a7abbb78c19b681338
Vladislav Yashin <v.yashin.work@gmail.com>
2018-12-26 13:50:35 +0400

    Bump version

--- travis.sh
+++ travis.sh
@@ -38,9 +38,9 @@ fi
 mv $TESTSPACE/database.yml.travis config/database.yml
 mv $TESTSPACE/additional_environment.rb config/
 
-# add telegram_common plugin
+# add redmine_bots plugin
 
-git clone -b develop git://github.com/centosadmin/redmine_bots.git $PATH_TO_REDMINE/plugins/redmine_bots
+git clone git://github.com/centosadmin/redmine_bots.git $PATH_TO_REDMINE/plugins/redmine_bots
 
 # create a link to the backlogs plugin
 ln -sf $PATH_TO_PLUGIN plugins/$NAME_OF_PLUGIN
