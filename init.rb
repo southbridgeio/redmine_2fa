11d5e7d32c540306629ca9d7ddcb68e8d04256c7
Vladislav Yashin <v.yashin.work@gmail.com>
2019-02-12 11:52:27 +0400

    Bump version [skip ci]

--- init.rb
+++ init.rb
@@ -23,7 +23,7 @@ end
 
 Redmine::Plugin.register :redmine_2fa do
   name 'Redmine 2FA'
-  version '1.7.0'
+  version '1.7.1'
   url 'https://github.com/centosadmin/redmine_2fa'
   description 'Two-factor authorization for Redmine'
   author 'Southbridge'
