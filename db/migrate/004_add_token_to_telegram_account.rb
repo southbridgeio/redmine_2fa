8c0959d141eaf538c8abce7e97295a094d30dd60
Vladislav Yashin <v.yashin.work@gmail.com>
2019-05-20 20:01:37 +0400

    Fix migrations

--- db/migrate/004_add_token_to_telegram_account.rb
+++ db/migrate/004_add_token_to_telegram_account.rb
@@ -1,7 +1,11 @@
+module Redmine2FA
+  class TelegramAccount < ActiveRecord::Base; end
+end
+
 class AddTokenToTelegramAccount < Rails.version < '5.0' ? ActiveRecord::Migration : ActiveRecord::Migration[4.2]
   def up
     add_column :redmine_2fa_telegram_accounts, :token, :string
-    RedmineTwoFa::TelegramAccount.find_each { |telegram_account| telegram_account.update_attribute(:token, ROTP::Base32.random_base32) }
+    Redmine2FA::TelegramAccount.find_each { |telegram_account| telegram_account.update_attribute(:token, ROTP::Base32.random_base32) }
   end
 
   def down
--- db/migrate/011_transfer_telegram_connections.rb
+++ db/migrate/011_transfer_telegram_connections.rb
@@ -1,6 +1,14 @@
+module Redmine2FA
+  module AuthSource
+    class SMS < ::AuthSource; end
+    class Telegram < ::AuthSource; end
+    class GoogleAuth < ::AuthSource; end
+  end
+end
+
 class TransferTelegramConnections < Rails.version < '5.0' ? ActiveRecord::Migration : ActiveRecord::Migration[4.2]
   def up
-    User.where(two_fa_id: RedmineTwoFa::AuthSource::Telegram.first.id).each do |user|
+    User.where(two_fa_id: Redmine2FA::AuthSource::Telegram.first.id).each do |user|
       next unless user.telegram_account
       RedmineTwoFa::TelegramConnection.create!(user_id: user.id, telegram_id: user.telegram_account.telegram_id)
     end
