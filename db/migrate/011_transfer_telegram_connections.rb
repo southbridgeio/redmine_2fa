40b289eda5fc479fcfd9d80749fe5e1cac78577f
Vladislav Yashin <v.yashin.work@gmail.com>
2019-05-20 20:40:13 +0400

    Fix migrations

--- db/migrate/011_transfer_telegram_connections.rb
+++ db/migrate/011_transfer_telegram_connections.rb
@@ -7,7 +7,17 @@ module Redmine2FA
 end
 
 class TransferTelegramConnections < Rails.version < '5.0' ? ActiveRecord::Migration : ActiveRecord::Migration[4.2]
+  class TelegramAccount
+    belongs_to :user
+  end
+
+  class User < Principal
+    has_one :telegram_account
+  end
+
   def up
+    return unless ActiveRecord::Base.connection.table_exists? 'telegram_accounts'
+
     User.where(two_fa_id: Redmine2FA::AuthSource::Telegram.first&.id).each do |user|
       next unless user.telegram_account
       RedmineTwoFa::TelegramConnection.create!(user_id: user.id, telegram_id: user.telegram_account.telegram_id)
