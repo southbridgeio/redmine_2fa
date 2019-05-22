0ea5d2be7ad85d83d2a0ac2b7783ceebc76cbbd3
Vladislav Yashin <v.yashin.work@gmail.com>
2019-05-22 14:50:59 +0400

    Fix zombie processes after sms send

--- lib/redmine_two_fa/protocols/sms.rb
+++ lib/redmine_two_fa/protocols/sms.rb
@@ -1,12 +1,20 @@
 module RedmineTwoFa::Protocols
   class Sms < BaseProtocol
+    TIMEOUT = 20
+
     def send_code(user)
       phone   = user.mobile_phone
       timestamp = Time.use_zone(user.time_zone) { 2.minutes.from_now.strftime('%H:%M:%S') }
       command = RedmineTwoFa::Configuration.sms_command
       command = command.sub('%{phone}', phone).sub('%{password}', user.otp_code).sub('%{expired_at}', timestamp)
 
-      Process.spawn(command)
+      pid = Process.spawn(command)
+
+      begin
+        Timeout.timeout(TIMEOUT) { Process.wait(pid) }
+      rescue Timeout::Error
+        Process.kill('TERM', pid)
+      end
     end
 
     def resendable?
