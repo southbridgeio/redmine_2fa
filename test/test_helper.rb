785d7c2d807fe8f25d0efe127176c5257b03d626
Vladislav Yashin <v.yashin.work@gmail.com>
2020-01-10 21:26:15 +0400

    Fix tests in CI

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,16 @@
+if RUBY_VERSION>='2.6.0'
+  if Rails.version < '5'
+    class ActionController::TestResponse < ActionDispatch::TestResponse
+      def recycle!
+        # hack to avoid MonitorMixin double-initialize error:
+        @mon_mutex_owner_object_id = nil
+        @mon_mutex = nil
+        initialize
+      end
+    end
+  end
+end
+
 $VERBOSE = nil # for hide ruby warnings
 
 # Load the Redmine helper
